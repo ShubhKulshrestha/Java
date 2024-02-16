947ebe380fac9bfa0ede15506d135d63141f59c3
ShubhKulshrestha <shubhamkulshrestha5196@gmail.com>
2024-02-16 22:54:09 +0530

    add.java

---
+++ add.java
@@ -0,0 +1,9 @@
+package java;
+
+public class add {
+        public static void main(String agrs[]){
+            int a = 5, b = 5,sum;
+            sum = a+b;
+            System.out.println(sum);
+        }
+}
