0e827a72be2338c0c59a080eb97c7b0896cfe742
Shubham Kulshrestha <144726609+ShubhKulshrestha@users.noreply.github.com>
2024-04-01 21:41:11 +0530

    Update EvenOdd.java

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,1 +1,12 @@
-
+import java.util.Scanner;
+public class EvenOdd {
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+     System.out.print("Enter a number: ");
+        int num = reader.nextInt();
+    if(num % 2 == 0)
+            System.out.println("number is"+num);
+        else
+            System.out.println("number is"+ num);
+    }
+}
