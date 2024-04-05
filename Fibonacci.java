900400e41d302b3bf76ffb13a205d86f6edf008c
Shubham Kulshrestha <144726609+ShubhKulshrestha@users.noreply.github.com>
2024-04-05 22:45:55 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,1 +1,13 @@
-
+//Fibonacci series..
+ public class fibonacci{
+    public static void main(String[] args) {
+        int i, n = 10, a = 0,b = 1;
+        System.out.print(a +" " +b+" ");
+        for (i = 1; i<=n; i++){
+            int fib = a + b;
+            a = b;
+            b = fib;
+            System.out.print(fib+" ");
+        }
+    }
+}
