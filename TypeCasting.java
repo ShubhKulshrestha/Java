78bc00ff7113023ddd412c9e616ab5575493d3c4
Shubham Kulshrestha <144726609+ShubhKulshrestha@users.noreply.github.com>
2024-04-16 22:39:36 +0530

    Update TypeCasting.java

--- TypeCasting.java
+++ TypeCasting.java
@@ -1,1 +1,8 @@
-
+public class TypeCasting {
+  public static void main(String[] args) {
+    int myInt = 9;
+    double myDouble = myInt;
+    System.out.println(myInt);      
+    System.out.println(myDouble);  
+  }
+}
