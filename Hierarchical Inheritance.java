4b34c40aef2e9c88075aa2c27fd9f3b0218dc0fd
Shubham Kulshrestha <144726609+ShubhKulshrestha@users.noreply.github.com>
2024-05-19 23:33:35 +0530

    Create Hierarchical Inheritance.java

---
+++ Hierarchical Inheritance.java
@@ -0,0 +1,27 @@
+// Hierarchical Inheritance in Java...
+class Animal {
+    void eat(){
+        System.out.println("Animal is eating");
+    }
+}
+class Dog extends Animal{
+    void bark(){
+        System.out.println("Dog is Barking");
+    }
+}
+class Cat extends Animal{
+    void cry(){
+        System.out.println("cat is crying");
+    }
+} 
+public class Test{
+    public static void main(String[] args) {
+        Cat c = new Cat();
+        c.cry();
+        c.eat();
+        Dog d = new Dog();
+        d.bark();
+        d.eat();
+        
+    }
+} 
