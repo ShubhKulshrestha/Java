7dc3fe1b42a34d88afbd983c71848750e6290776
Shubham Kulshrestha <144726609+ShubhKulshrestha@users.noreply.github.com>
2024-05-20 23:14:35 +0530

    Update Single Inheritance.java

--- Single Inheritance.java
+++ Single Inheritance.java
@@ -1,1 +1,19 @@
+// Single Inheritance
+class Animal {
+    void eat() {
+        System.out.println("This animal eats food.");
+    }
+}
+class Dog extends Animal {
+    void bark() {
+        System.out.println("The dog barks.");
+    }
+}
+public class Test {
+    public static void main(String[] args) {
+      Dog myDog = new Dog();
+      myDog.bark();
+      myDog.eat(); 
+    }
+}
 
