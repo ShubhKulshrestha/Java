813eb5a210b70c571a3c63a014131bf22c98dd83
Shubham Kulshrestha <144726609+ShubhKulshrestha@users.noreply.github.com>
2024-05-20 23:19:23 +0530

    Update Multilevel Inheritance.java

--- Multilevel Inheritance.java
+++ Multilevel Inheritance.java
@@ -1,1 +1,25 @@
+// Multilevel Inheritance
+class Animal {
+    void eat() {
+        System.out.println("This animals are eating");
+    }
+}
+class Dog extends Animal {
+    void bark() {
+        System.out.println("The dog is barking.");
+    }
+}
+class Cat extends Dog {
+    void cry() {
+        System.out.println("The cat is crying");
+    }
+}
+public class Test {
+    public static void main(String[] args) {
+        Cat mycat = new Cat();
+        mycat.cry();
+        mycat.bark();
+        mycat.eat();
+    }
+}
 
