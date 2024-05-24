b7bb4e8ccf0543cc73936bd9da50ec8e8543c1fb
Shubham Kulshrestha <144726609+ShubhKulshrestha@users.noreply.github.com>
2024-05-24 21:38:57 +0530

    Update Input.java

--- Input.java
+++ Input.java
@@ -3,7 +3,7 @@ import package java.util.Scanner; // // also use java .util.*;
   class input{
   public static void main ( String args[]){
     int a;
-    System.out.println("Enter the number");
+    
     Scanner sc = new Scanner ( System.in);
     System.out.println("Enter the Number");
     a = sc.nextInt();
